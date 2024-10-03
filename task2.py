97a97461509311759a5c5517ea985327b335915e
Jasper2m <jasperabocvirtudazo@gmail.com>
2024-10-03 11:57:41 +0800

    firs task

---
+++ task2.py
@@ -0,0 +1,9 @@
+list_of_fruits = ["banana", "apple", "orange", "mango"]
+
+for fruits in list_of_fruits:
+    if fruit == "banana" :
+        print("Hello banana")
+    elif fruit == "apple" :
+        print("Hello apple")
+    else:
+        print("no")
